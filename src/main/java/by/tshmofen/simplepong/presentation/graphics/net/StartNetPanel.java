b5f06c54d63fcad02b48ed80a0c97fcf330ffd92
Timofey <timohatimafeikq@gmail.com>
2020-05-25 15:32:19 +0300

    add connect and start multiplayer menus

---
+++ src/main/java/by/tshmofen/simplepong/presentation/graphics/net/StartNetPanel.java
@@ -0,0 +1,77 @@
+package by.tshmofen.simplepong.presentation.graphics.net;
+
+import by.tshmofen.simplepong.domain.AppTabs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class StartNetPanel extends JPanel {
+    private final int width;
+    private final int height;
+
+    private final JTextField portField;
+
+    public StartNetPanel() {
+        this.grabFocus();
+        width = AppTabs.frame.getContentPane().getWidth();
+        height = AppTabs.frame.getContentPane().getHeight();
+        int menuWidth = 300;
+
+        ArrayList<JButton> buttons = new ArrayList<>();
+        buttons.add(new JButton("Start Local Game"));
+        buttons.add(new JButton("Return"));
+        setDesign(buttons, menuWidth);
+
+        JLabel portLabel = new JLabel("Port");
+        portField = new JTextField("8080");
+        portLabel.setBounds(width / 2 - menuWidth/2, height * 3 / 12,
+                menuWidth / 5 - 5, height / 12 - 5);
+        portLabel.setFont(new Font("Courier", Font.BOLD, 22));
+        portLabel.setForeground(Color.LIGHT_GRAY);
+        portField.setBounds(width / 2 - menuWidth/2 + menuWidth / 5, height * 3 / 12,
+                menuWidth * 4 / 5 - 5, height / 12 - 5);
+        portField.setFont(new Font("Courier", Font.BOLD, 22));
+
+        this.setBackground(Color.DARK_GRAY);
+        for (JButton button: buttons) {
+            this.add(button);
+        }
+        this.add(portLabel);
+        this.add(portField);
+        this.setLayout(null);
+        this.setSize(width, height);
+
+        startListening(buttons);
+    }
+
+    private void setDesign(ArrayList<JButton> buttons, int menuWidth) {
+        for (int i = 0; i < buttons.size(); i++){
+            JButton button = buttons.get(i);
+            button.setBounds(width / 2 - menuWidth/2
+                    , height * (2*i + 2) / 12, menuWidth, height / 12 - 5);
+            button.setBackground(Color.LIGHT_GRAY);
+            button.setForeground(Color.DARK_GRAY);
+            button.setFont(new Font("Courier", Font.BOLD, 22));
+        }
+    }
+
+    private void startListening(ArrayList<JButton> buttons) {
+        buttons.get(0).addActionListener(e -> {
+            try {
+                int port = Integer.parseInt(portField.getText());
+                AppTabs.frame.setContentPane(AppTabs.pong);
+                AppTabs.frame.setVisible(true);
+                AppTabs.pong.startTheGame();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(AppTabs.frame, "Incorrect port. Try again",
+                        "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+
+        buttons.get(1).addActionListener(e -> {
+            AppTabs.frame.setContentPane(AppTabs.menu);
+            AppTabs.frame.setVisible(true);
+        });
+    }
+}
